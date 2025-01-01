028e9e7c2427437a1e9940976c3b0ecd3eaaed3f
Aleksandr Platcev <a.platcev@ya.ru>
2025-01-01 13:04:30 +0300

    Добавлено подключение к БД

---
+++ libs/db/db.ts
@@ -0,0 +1,9 @@
+import { Pool } from 'pg';
+
+export const pool = new Pool({
+  user: process.env.DB_USER,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+  host: process.env.DB_HOST,
+});
